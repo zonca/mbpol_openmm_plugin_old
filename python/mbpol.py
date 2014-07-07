2248b289152a953f8539208b47cb0f1d97dfe65c
Andrea Zonca <code@andreazonca.com>
2014-07-07 10:59:30 -0700

    Implementation of ElectrostaticsForce generator

--- python/mbpol.py
+++ python/mbpol.py
@@ -251,3 +251,100 @@ class MBPolDispersionForceGenerator:
                 force.addParticle(v)
 
 forcefield.parsers["MBPolDispersionForce"] = MBPolDispersionForceGenerator.parseElement
+
+## @private
+class MBPolElectrostaticsForceGenerator:
+
+    def __init__(self):
+
+        self.types1 = []
+        self.types2 = []
+        self.types3 = []
+        self.typeMap = {}
+
+    @staticmethod
+    def parseElement(element, forceField):
+
+        # <MBPolElectrostaticsForce>
+        #     <Residue class1="OW" class2="HW" class3="HW" thole-charge-charge="0.4" thole-charge-dipole="0.4" thole-dipole-dipole-intermolecules="0.055" thole-dipole-dipole-1-2="0.055" thole-dipole-dipole-1-3="0.626" thole-dipole-dipole-2-3="0.626" /> 
+        #     <Atom type="MBPol-O" charge="-5.1966000e-01" damping-factor="0.00131" polarizability="0.00131" />
+        #     <Atom type="MBPol-H" charge="2.5983000e-01" damping-factor="0.000294" polarizability="0.000294" />
+        #     <Atom type="MBPol-M" charge="0" damping-factor="0.00131" polarizability="0" />
+        # </MBPolElectrostaticsForce>
+
+        generator = MBPolElectrostaticsForceGenerator()
+        forceField._forces.append(generator)
+
+        for MBPolElectrostaticsForce_template in element.findall('MBPolElectrostaticsForce'):
+            types = forceField._findAtomTypes(MBPolElectrostaticsForce_template, 3)
+            if None not in types:
+
+                generator.types1.append(types[0])
+                generator.types2.append(types[1])
+                generator.types3.append(types[2])
+
+            else:
+                outputString = self.__class__ + ": error getting types: %s %s %s" % (
+                                    MBPolElectrostaticsForce_template.attrib['class1'],
+                                    MBPolElectrostaticsForce_template.attrib['class2'],
+                                    MBPolElectrostaticsForce_template.attrib['class3'])
+                raise ValueError(outputString)
+
+        for residue in element.findall('Residue'):
+
+        #     <Residue name="HOH" class1="OW" class2="HW" class3="HW" thole-charge-charge="0.4" thole-charge-dipole="0.4" thole-dipole-dipole-intermolecules="0.055" thole-dipole-dipole-1-2="0.055" thole-dipole-dipole-1-3="0.626" thole-dipole-dipole-2-3="0.626" /> 
+
+            name = residue.attrib["name"]
+
+            v = mbpolplugin.vectord()
+            thole_components = ["thole-charge-charge", "thole-charge-dipole", "thole-dipole-dipole-intermolecules", "thole-dipole-dipole-OH", "thole-dipole-dipole-HH", ]
+            for each in thole_components:
+                v.push_back(float(residue.attrib[each]))
+
+            generator.typeMap[name] = dict(thole = v)
+
+        for atom in element.findall('Atom'):
+        #     <Atom type="MBPol-H" charge="2.5983000e-01" damping-factor="0.000294" polarizability="0.000294" />
+            types = forceField._findAtomTypes(atom, 1)
+            if None not in types:
+
+                for t in types[0]:
+                    generator.typeMap[t] = dict(  polarizability = float(atom.attrib['polarizability']),
+                                             charge         = float(atom.attrib['charge']),
+                                             damping_factor = float(atom.attrib['damping-factor']) )
+
+            else:
+                outputString = "MBPolElectrostaticsForceGenerator: error getting type for atom: %s" % (atom.attrib['class'])
+                raise ValueError(outputString)
+
+    def createForce(self, sys, data, nonbondedMethod, nonbondedCutoff, args):
+
+        existing = [sys.getForce(i) for i in range(sys.getNumForces())]
+        existing = [f for f in existing if type(f) == mbpolplugin.MBPolElectrostaticsForce]
+
+        if len(existing) == 0:
+            force = mbpolplugin.MBPolElectrostaticsForce()
+            sys.addForce(force)
+        else:
+            force = existing[0]
+
+        for i in range(len(data.angles)):
+            angle = data.angles[i]
+            # FIXME loop through all residues of MBPolElectrostaticsForce and match their name
+
+            # FIXME cheating! get virtual site index by max(otheratom indices)  + 1
+            global_atoms_indices = set([angle[local_atom_index] for local_atom_index in [0,1,2]])
+            virtual_site_index = max(global_atoms_indices) + 1
+            global_atoms_indices.add(virtual_site_index)
+
+            for atomIndex in global_atoms_indices:
+                atom = data.atoms[atomIndex]
+                t = data.atomType[atom]
+                if t in self.typeMap:
+                    other_atoms = global_atoms_indices - set([atomIndex])
+                    force.addElectrostatics(self.typeMap[t]['charge'], data.atoms[other_atoms.pop()].index, data.atoms[other_atoms.pop()].index, data.atoms[other_atoms.pop()].index, self.typeMap[atom.residue.name]['thole'], self.typeMap[t]['damping_factor'], self.typeMap[t]['polarizability'])
+
+                else:
+                    raise ValueError('No type for atom %s %s %d' % (atom.name, atom.residue.name, atom.residue.index))
+
+forcefield.parsers["MBPolElectrostaticsForce"] = MBPolElectrostaticsForceGenerator.parseElement
